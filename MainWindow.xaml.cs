b35b45b86c01764ff8b76b0da647314d4f3df48c
Kieran Coldron <kieran@coldron.com>
2022-09-01 02:18:58 +0100

    Initial prototype

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace StepDownWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddMudServices();
+#if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
