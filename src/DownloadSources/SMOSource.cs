16da5fad8ed717505f9cf2eebbac7d43878443fd
Kieran Coldron <kieran@coldron.com>
2022-09-13 01:29:03 +0100

    begin smo seach and download progress

--- src/DownloadSources/SMOSource.cs
+++ src/DownloadSources/SMOSource.cs
@@ -1,4 +1,5 @@
-﻿using AngleSharp;
+﻿using System.IO.Compression;
+using AngleSharp;
 using StepManager.Services;
 
 namespace StepManager.DownloadSources;
@@ -34,48 +35,48 @@ public class SMOSource
     }
 
     
-    public async Task<List<(string name, string id, string pack, string packid)>> SearchPacks(string title, string artist)
+    public async Task<IEnumerable<SongDetails>> SearchPacksTitle(string title)
     {
-        try
-        {
-            var catListUrl = "https://zenius-i-vanisher.com/v5.2/simfiles_search_ajax.php";
-            var data = new FormUrlEncodedContent(new Dictionary<string, string>
-                {
-                    { "songtitle", title },
-                    { "songartist", artist }
-                }
-            );
-            var response = await HttpClient.PostAsync(catListUrl, data);
-            using var document =
-                await context.OpenAsync(async req => req.Content(await response.Content.ReadAsStringAsync()));
-            var rows = document.QuerySelectorAll("tr");
-            
-            if (rows != null && rows.Any()) {
-                return rows.Where(x=> x.FirstElementChild?.NodeName == "TD" && x.FirstElementChild.ChildElementCount > 0).Select(x => (
-                    name: x.Children[0].FirstElementChild.TextContent,
-                    id: x.Children[0].FirstElementChild.GetAttribute("href").Split('=')[1],
-                    pack: x.Children[1].FirstElementChild.TextContent,
-                    packid: x.Children[1].FirstElementChild.GetAttribute("href").Split('=')[1]
-                )).ToList();}
-            
-        }
-        catch (Exception ex)
-        {
-        }
-        return new List<(string name, string id, string pack, string packid)>();
+            var catListUrl = $"https://search.stepmaniaonline.net/title/{title}";
 
+            var document = await context.OpenAsync(catListUrl);
+
+            var simTdSelector = "html body div.container table.table tbody tr";
+
+            var SongList = document.QuerySelectorAll(simTdSelector).Select(x =>
+                new SongDetails(
+                    title: x.Children[0].TextContent,
+                    artist: x.Children[1].TextContent,
+                    subTitle: x.Children[2].TextContent,
+                    credit: x.Children[3].TextContent,
+                    banner: x.Children[4].TextContent,
+                    lastUpdate: DateTime.Parse(x.Children[5].TextContent)
+                )
+            );
+            
+            return SongList;
     }
 
-    async Task DownloadBanner(string bannerUrl, string savePath)
+    public async Task<IEnumerable<SongDetails>> SearchPacksArtist(string artist)
     {
-        await using var bannerStream = await HttpClient.GetStreamAsync(bannerUrl);
+        var catListUrl = $"https://search.stepmaniaonline.net/artist/{artist}";
 
-        var ext = Path.GetExtension(bannerUrl);
-        if (ext.Contains('?')) ext = ext.Split('?')[0];
-        
-        var banner = Path.Join(savePath, $"banner{ext}");
-        await using var bannerfs = new FileStream(banner, FileMode.CreateNew);
-        await bannerStream.CopyToAsync(bannerfs);
+        var document = await context.OpenAsync(catListUrl);
+
+        var simTdSelector = "html body div.container table.table tbody tr";
+
+        var SongList = document.QuerySelectorAll(simTdSelector).Select(x =>
+            new SongDetails(
+                title: x.Children[0].TextContent,
+                artist: x.Children[1].TextContent,
+                subTitle: x.Children[2].TextContent,
+                credit: x.Children[3].TextContent,
+                banner: x.Children[4].TextContent,
+                lastUpdate: DateTime.Parse(x.Children[5].TextContent)
+            )
+        );
+            
+        return SongList;
     }
 
     public async Task<PackDetails> GetPackDetails(string id)
@@ -99,15 +100,13 @@ public class SMOSource
                 lastUpdate: DateTime.Parse(x.Children[5].TextContent)
             )
         );
-        var imgUrl = document.QuerySelector("html body div.fixwidth div.content p.centre img")?.GetAttribute("src");
-        imgUrl = imgUrl?.Replace("..", "https://zenius-i-vanisher.com/");
 
-        
-        return new PackDetails(title, id,imgUrl, SongList);
-
-
+        return new PackDetails(title, id, null, SongList);
     }
-    public async Task DownloadPack(PackDetails pack)
+    
+    float GetProgressPercentage (float totalBytes, float currentBytes) => (totalBytes / currentBytes) * 100f;
+    
+    public async Task DownloadPack(PackDetails pack, IProgress<float>? progress, CancellationToken cancellationToken = default)
     {
         // Snackbar.Add($"Downloading pack {Title}", Severity.Info);
         
@@ -117,14 +116,60 @@ public class SMOSource
         var downloadUrl = $"https://search.stepmaniaonline.net{document.QuerySelector("html body div.container h4 a")?.GetAttribute("href")}";
 
         var response = await HttpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, downloadUrl)
-            { Headers = { Referrer = new Uri(downloadUrl) } });
-
-
-        await using var s = await response.Content.ReadAsStreamAsync();
-
+        {
+            Headers = { Referrer = new Uri(downloadUrl) } 
+            
+        });
         var zip = Path.Join(SettingsService.Settings.StepManiaSongPath, $"{pack.Id}.zip");
-        await using var fs = new FileStream(zip, FileMode.CreateNew);
-        await s.CopyToAsync(fs);
-        // File.Delete(zip);
+
+        await using (var fs = new FileStream(zip, FileMode.CreateNew))
+        {
+
+            var contentLength = response.Content.Headers.ContentLength;
+            using (var download = await response.Content.ReadAsStreamAsync(cancellationToken))
+            {
+                // no progress... no contentLength... very sad
+                if (progress is null || !contentLength.HasValue)
+                {
+                    await download.CopyToAsync(fs);
+                    return;
+                }
+
+                // Such progress and contentLength much reporting Wow!
+                var progressWrapper = new Progress<long>(totalBytes =>
+                    progress.Report(GetProgressPercentage(totalBytes, contentLength.Value)));
+                await CopyToAsync(download, fs, 81920, progressWrapper, cancellationToken);
+            }
+        }
+
+        ZipFile.ExtractToDirectory(zip, SettingsService.Settings.StepManiaSongPath, true);
+        
+
+        File.Delete(zip);
+    }
+    
+    
+    
+    static async Task CopyToAsync (Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default (CancellationToken))
+    {
+        if (bufferSize < 0)
+            throw new ArgumentOutOfRangeException (nameof (bufferSize));
+        if (source is null)
+            throw new ArgumentNullException (nameof (source));
+        if (!source.CanRead)
+            throw new InvalidOperationException ($"'{nameof (source)}' is not readable.");
+        if (destination == null)
+            throw new ArgumentNullException (nameof (destination));
+        if (!destination.CanWrite)
+            throw new InvalidOperationException ($"'{nameof (destination)}' is not writable.");
+
+        var buffer = new byte[bufferSize];
+        long totalBytesRead = 0;
+        int bytesRead;
+        while ((bytesRead = await source.ReadAsync (buffer, 0, buffer.Length, cancellationToken).ConfigureAwait (false)) != 0) {
+            await destination.WriteAsync (buffer, 0, bytesRead, cancellationToken).ConfigureAwait (false);
+            totalBytesRead += bytesRead;
+            progress?.Report (totalBytesRead);
+        }
     }
 }
